d7618f56209d883d79489a6bd5d65fae25061644
officialdiwakar223@gmail.com <officialdiwakar223@gmail.com>
2022-07-11 23:00:25 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("features-show");
+}
+
+function myFunction2(){
+  document.getElementById("mydropdown2").classList.toggle("career-show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
